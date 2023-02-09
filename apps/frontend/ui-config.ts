cdae2ab6637d66d50483296d749c383d9865b0c6
jonathan.lee <jonathan.lee@slalom.com>
2023-02-09 10:08:15 -0500

    (refactor) Fixing env variables. Forgot I wasn't using dotenv. NextJS standard handling is fine for this.

--- apps/frontend/ui-config.ts
+++ apps/frontend/ui-config.ts
@@ -1,7 +1,9 @@
 type UIConfig = {
-  PUBLIC_API: string;
+  NEXT_PUBLIC_API: string;
+  NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: string;
 };
 
 export const uiConfig: UIConfig = {
-  PUBLIC_API: process.env.PUBLIC_API || 'localhost:3333',
+  NEXT_PUBLIC_API: process.env.NEXT_PUBLIC_API || 'localhost:3333',
+  NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
 };
