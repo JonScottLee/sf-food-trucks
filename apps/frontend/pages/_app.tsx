afac4195e97eba1c0b56c2d8d408491904377f1a
jonathan.lee <jonathan.lee@slalom.com>
2023-02-09 13:40:59 -0500

    (feat) Applying a basic Tailwind theme, because the white is making my eyes bleed.

--- apps/frontend/pages/_app.tsx
+++ apps/frontend/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
+import { Nav } from '../components/nav/nav';
 import './styles.css';
 
 function CustomApp({ Component, pageProps }: AppProps) {
@@ -8,8 +9,13 @@ function CustomApp({ Component, pageProps }: AppProps) {
       <Head>
         <title>Welcome to the Food Truck Finder App</title>
       </Head>
+      <Nav />
       <main className="app">
-        <Component {...pageProps} />
+        <div className="pt-24">
+          <div className="container px-3 mx-auto flex flex-wrap flex-col md:flex-row items-center">
+            <Component {...pageProps} />
+          </div>
+        </div>
       </main>
     </>
   );
--- apps/frontend/pages/index.tsx
+++ apps/frontend/pages/index.tsx
@@ -1,20 +1,33 @@
-import { useFetchTrucks } from '../hooks/use-fetch-trucks';
-import { Card } from '../components/card/card';
+import Link from 'next/link';
+import Image from 'next/image';
 
 export function Index() {
-  const { isFetching, foodTruckData } = useFetchTrucks();
-
-  if (isFetching) return <div>Loading...</div>;
-
   return (
     <>
-      <h1>Home</h1>
-
-      {Object.keys(foodTruckData).map((key, idx) => {
-        const truck = foodTruckData[key];
-
-        return <Card key={`${truck.objectid}-${idx}`} {...truck} />;
-      })}
+      <div className="flex flex-col w-full md:w-2/5 justify-center items-start text-center md:text-left">
+        <p className="uppercase tracking-loose w-full">San Francisco</p>
+        <h1 className="my-4 text-5xl font-bold leading-tight">
+          Where is your favorite food truck?
+        </h1>
+        <p className="leading-normal text-2xl mb-8">
+          Find all your favorite local food trucks!
+        </p>
+        <Link
+          href="/all-trucks"
+          className="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out"
+        >
+          See Food Trucks
+        </Link>
+      </div>
+      <div className="w-full md:w-3/5 py-6 text-center">
+        <Image
+          alt="picture of a food truck | Image by rawpixel.com"
+          className="w-full md:w-4/5 z-50"
+          height="520"
+          src="/hero.png"
+          width="602"
+        />
+      </div>
     </>
   );
 }
