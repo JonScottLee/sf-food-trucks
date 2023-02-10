1866404aafc391e0a82d7e52b4dd394344264697
jonathan.lee <jonathan.lee@slalom.com>
2023-02-10 13:25:37 -0500

    (test) Button tests.

--- apps/frontend/components/breadcrumb/breadcrumb.spec.tsx
+++ apps/frontend/components/breadcrumb/breadcrumb.spec.tsx
@@ -5,9 +5,12 @@ import { Breadcrumb } from './breadcrumb';
 
 describe('Breadcrumb', () => {
   it('should render successfully', () => {
+    // Act
     const { baseElement } = render(
       <Breadcrumb routes={routes} pathName="all-trucks" />
     );
+
+    // Assert
     expect(baseElement).toBeTruthy();
   });
 });
